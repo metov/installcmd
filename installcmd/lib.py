257089fc6f31bce91fe2094b54c7afc6e9f2d717
Azat <azatinfo@yandex.com>
2021-07-05 14:01:45 -0700

    rewrite spec search logic

--- installcmd/lib.py
+++ installcmd/lib.py
@@ -1,8 +1,10 @@
 import platform
 from pathlib import Path
-from typing import Dict, Union
+from typing import Dict, Union, List
 
 import yaml
+
+from installcmd import log
 
 RELEASE_SPEC = Dict[str, str]
 DISTRO_SPEC = Dict[str, Union[str, RELEASE_SPEC]]
@@ -24,7 +26,8 @@ def install_command(pkg_spec: Dict[str, OS_SPEC] = None) -> str:
     cmd_spec = load_yaml(Path(__file__).parent / "commands.yaml")
     install = apply_spec(cmd_spec, "install")
     if install is None:
-        raise Exception("Could not find valid install command.")
+        log.error(f"No known install command for this platform")
+        exit(1)
 
     if pkg_spec is None:
         return install
@@ -34,7 +37,7 @@ def install_command(pkg_spec: Dict[str, OS_SPEC] = None) -> str:
     if package is None:
         raise Exception("Could not find valid package name.")
 
-    return f'{install} {package}'
+    return f"{install} {package}"
 
 
 def update_command() -> str:
@@ -54,26 +57,49 @@ def update_command() -> str:
 
 
 def apply_spec(spec: Dict[str, OS_SPEC], key: str) -> str:
-    # Read OS, distribution and release
+    """
+    Read OS, distribution and release.
 
-    u = platform.uname()
+    The implementation uses os/distro/release terminology, which is Linux-specific.
+    However we are actually calling Python's platfrom uname, node and release which
+    should be platform independent (at least in theory). Since Linux OSes have
+    vastly more version diversity, there's not really a commonly used Kernel-agnostic
+    terminology, so I decided to just pretend the Linux terms apply to all OSes. Again,
+    this is a matter of comments and variable names, as the code itself is meant
+    to be OS-agnostic.
+    """
+    # Try looking for successively less qualified commands
+    path = [platform.uname().system, distro_name(), platform.uname().release]
+    while path:
+        str_path = "/".join(path)
+        release_cmd = dict_get_path(spec, path + [key])
+        if release_cmd:
+            log.info(f'Found "{release_cmd}" at {str_path}')
+            return release_cmd
+        log.debug(f"Couldn't find key: {str_path}")
+        path.pop()
 
-    # Default value
-    res = spec.get(key, None)
+    # At this point, you would return an OS-independent generic command, but if
+    # such a thing existed this package would not be necessary :)
+    log.error(f"Couldn't match this platform. Returning None.")
+    return None
 
-    # Per-OS value
-    d_os = spec.get(u.system, {})
-    res = d_os.get(key, res)
 
-    # Per-distro value
-    d_dist = d_os.get(u.node, {})
-    res = d_dist.get(key, res)
+def distro_name():
+    # Doesn't work on Docker - instead, if Linux, check /etc/os-release
+    return platform.uname().node
 
-    # Per release value
-    d_rls = d_dist.get(u.release, {})
-    res = d_rls.get(key, res)
 
-    return res
+def dict_get_path(d: dict, key_path: List[str]):
+    """
+    Descend down the path of keys in the dictionary and retrieve value. If not
+    found, return None.
+    """
+    for k in key_path:
+        if k not in d:
+            return None
+        d = d[k]
+    return d
 
 
 def load_yaml(yaml_path: str):
