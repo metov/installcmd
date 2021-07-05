a704ff1f397f107c72a970bf157e450a4b9d6dd3
Azat <azatinfo@yandex.com>
2021-07-05 12:26:34 -0700

    use pathlib for more concise readme load

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
+from pathlib import Path
+
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
 
 setuptools.setup(
     name="installcmd-metov",
     version="0.1.1",
     author="Azat Akhmetov",
     description="Print correct command for installing a package.",
-    long_description=long_description,
+    long_description=Path("README.md").read_text(),
     long_description_content_type="text/markdown",
     url="https://github.com/metov/installcmd",
     packages=setuptools.find_packages(),
