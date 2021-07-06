f17470ff75994084fd4bbc56f052cba69010c617
Azat <azatinfo@yandex.com>
2021-07-05 20:41:47 -0700

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name="installcmd",
-    version="0.3.1",
+    version="0.4.0",
     author="Azat Akhmetov",
     description="Print correct command for installing a package.",
     long_description=Path("README.md").read_text(),
