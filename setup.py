4591557b5bd9e5d71485f7eb9cbec6e1a047587f
Azat Akhmetov <51468504+metov@users.noreply.github.com>
2022-06-30 23:11:02 -0600

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name="installcmd",
-    version="0.4.6",
+    version="0.4.7",
     author="Azat Akhmetov",
     description="Print correct command for installing a package.",
     long_description=Path("README.md").read_text(),
